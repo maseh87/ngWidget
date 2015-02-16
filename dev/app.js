6d558c664ae0326744e91094ba9ca1478ab597e3
Hendrixer <willscottmoss@gmail.com>
2015-02-15 22:36:09 -0800

    fix bugs around scope

--- dev/app.js
+++ dev/app.js
@@ -1,8 +1,15 @@
 angular.module("app", ['ngWidget'])
 .directive('demo', function(Widget) {
   var widget = new Widget();
+
   widget.on('click', function(e, scope){
     scope.message = 'changed';
+
   });
+
+  widget.scopeOptions({
+    'code': 'one-way'
+  });
+
   return widget;
 });
